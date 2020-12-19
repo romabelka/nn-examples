d0a49af96e84dfaea3201b85f92af7abf8a5c273
Roman Iakobchuk <romania@wix.com>
2020-12-20 01:10:48 +0200

    add softmax dropout

--- digits/dropout_softmax.py
+++ digits/dropout_softmax.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 n_labels = 10
 MAX_EXP_ARG = 500
+keep_prob = 0.8
 
 data = np.genfromtxt('digits/datasets/train.csv', delimiter=',')
 
@@ -30,8 +31,6 @@ Y_test = Y[:, m_train + m_dev :]
 
 n_x = X.shape[0]
 
-print(data.shape)
-
 print("m_train", m_train, ". m_test", m_test)
 
 def softmax(z):
@@ -49,23 +48,27 @@ def init_params(n_x, h_layers, n_l):
         params["b" + str(l)] = np.zeros((all_layers[l], 1))
     return params
 
-def forward_step(params, h_layers, X):
+def forward_step(params, h_layers, X, keep_prob):
     A = X
     depth = len(h_layers) + 2
-    cache = {"A0": A, "Z0": X}
+    cache = {"A0": A, "Z0": X, "D0": []}
 
     for l in range(1, depth):
         W = params["W" + str(l)]
         b = params["b" + str(l)]
 
         z = np.dot(W, A) + b
+
+        D = (np.random.rand(z.shape[0], z.shape[1]) < keep_prob).astype(int)
+
         if (l == depth - 1):
             A = softmax(z)
         else:
-            A = relu(z)
+            A = relu(z) * D / keep_prob
 
         cache["A" + str(l)] = A
         cache["Z" + str(l)] = z
+        cache["D" + str(l)] = D
 
 
     return A, cache
@@ -90,9 +93,9 @@ def relu_backward(dA, Z):
         
     return dZ 
 
-def grad_descend(cache, X, Y, params, lambd):
+def get_grads(cache, X, Y, params, lambd, keep_prob):
     m = X.shape[1]
-    L = len(cache) // 2 - 1
+    L = len(cache) // 3 - 1
     grads = {}
     A = cache["A" + str(L)]
     dZ = A - Y # (n_l,m)
@@ -100,13 +103,17 @@ def grad_descend(cache, X, Y, params, lambd):
     for l in range(L, 0, -1):
         A_prev = cache["A" + str(l - 1)]
         W = params["W" + str(l)]
+
         dw = np.dot(dZ, A_prev.T) / m + W * lambd / m # (n_l, m) * (m, n) = (n_l, n)
         db = np.sum(dZ, axis=1, keepdims=True) / m
             
-        dA = np.dot(W.T, dZ)
         grads["dW" + str(l)] = dw 
         grads["db" + str(l)] = db
-        dZ = relu_backward(dA, cache["Z" + str(l - 1)])
+
+        if (l > 1):
+            D_prev = cache["D" + str(l - 1)]
+            dA = np.dot(W.T, dZ) * D_prev / keep_prob
+            dZ = relu_backward(dA, cache["Z" + str(l - 1)])
 
     return grads
 
@@ -160,12 +167,13 @@ def show_correctness(params, h_layers):
         print("TEST. Correctness: ", test_correctness, " %")
 
 
-def model_adam(X, Y, num_epochs, lambd, learning_rate, h_layers, minibatch_size):
+def model_adam(X, Y, num_epochs, lambd, keep_prob, learning_rate, h_layers, minibatch_size):
     params = init_params(X.shape[0], h_layers, n_labels)
     minibatches = n_utils.get_minibatches(X, Y, minibatch_size)
     l_dacay_rate = 0.01
     t = 0
     v, s = init_adam(params)
+    costs = []
 #    best_params = {}
 #    best_test_perf = 0
 
@@ -175,9 +183,9 @@ def model_adam(X, Y, num_epochs, lambd, learning_rate, h_layers, minibatch_size)
         for minibatch in minibatches:
             t += 1
             X_mini, Y_mini = minibatch
-            Y_hat, cache = forward_step(params, h_layers, X_mini)
+            Y_hat, cache = forward_step(params, h_layers, X_mini, keep_prob = keep_prob)
             cost_total += compute_cost(Y_hat, Y_mini, params, lambd)
-            grads = grad_descend(cache, X_mini, Y_mini, params, lambd)
+            grads = get_grads(cache, X_mini, Y_mini, params, lambd, keep_prob = keep_prob)
             params, v, s = update_params_adam(params, grads, v, s, t, learning_rate / (1 + l_dacay_rate * i), beta1 = 0.8)
 
 #        test_correctness = get_correctness(predict(X_test, h_layers, params), Y_test)
@@ -185,38 +193,43 @@ def model_adam(X, Y, num_epochs, lambd, learning_rate, h_layers, minibatch_size)
 #            best_test_perf = test_correctness
 #            best_params = params
 
+        costs.append(cost_total)
+
         if (i % 50 == 0):
             show_correctness(params, h_layers)
             print("cost after " + str(i) + ": ", cost_total, ". Learning rate: ", learning_rate / (1 + l_dacay_rate * i))
 
-    return params
+    return params, costs
 
 
-def model(X, Y, num_epochs = 500, lambd = 0.1, learning_rate=0.1, h_layers=[2, 5, 4], minibatch_size=128):
+def model(X, Y, num_epochs = 500, lambd = 0.1, learning_rate=0.1, h_layers=[2, 5, 4], minibatch_size=128, keep_prob=0.8):
     params = init_params(X.shape[0], h_layers, n_labels)
     minibatches = n_utils.get_minibatches(X, Y, minibatch_size)
     l_dacay_rate = 0.001
+    costs = []
 
     for i in range(0, num_epochs + 1):
         cost_total = 0
 
         for minibatch in minibatches:
             X_mini, Y_mini = minibatch
-            Y_hat, cache = forward_step(params, h_layers, X_mini)
+            Y_hat, cache = forward_step(params, h_layers, X_mini, keep_prob = keep_prob)
             cost_total += compute_cost(Y_hat, Y_mini, params, lambd)
-            grads = grad_descend(cache, X_mini, Y_mini, params, lambd)
+            grads = get_grads(cache, X_mini, Y_mini, params, lambd, keep_prob = keep_prob)
             params = update_params(params, grads, learning_rate / (1 + l_dacay_rate * i))
             if (np.isnan(cost_total)):
                 print("Nan after iteration: ", i, grads, params)
                 return params
 
+        costs.append(cost_total)
+
         if (i % 100 == 0):
             print("cost after " + str(i) + ": ", cost_total, ". Learning rate: ", learning_rate / (1 + l_dacay_rate * i))
 
-    return params
+    return params, costs
 
 def predict(X, h_layers, params):
-    a, _ = forward_step(params, h_layers, X)
+    a, _ = forward_step(params, h_layers, X, keep_prob = 1)
     return (a == np.max(a, axis=0)) * 1
 
 def get_correctness(predict, Y):
@@ -227,32 +240,42 @@ def get_correctness(predict, Y):
     acuracy = 100 * np.sum(np.equal(predict_res, truth)) / total
     return acuracy
 
-def tune_hyper_params(architectures = [[10], [20], [10, 5], [20, 5]], lambds = [0, 0.1, 1], iterations = 1000, learning_rate = 0.001):
+def plot_cost(costs):
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (x1,000)')
+    plt.show()
+
+def tune_hyper_params(architectures = [[10], [20], [10, 5], [20, 5]], lambds = [0], keep_probs= [0], iterations = 1000, learning_rate = 0.001):
     for i in range(0, len(architectures)):
         h_layers = architectures[i]
         for j in range(0, len(lambds)):
-            lambd = lambds[j]
+            for keep_prob in keep_probs:
+                lambd = lambds[j]
 
-            params = model_adam(X_train, Y_train, num_epochs = iterations, minibatch_size = 128, learning_rate = learning_rate, lambd = lambd, h_layers = h_layers)            
-            #params = model(X_train, Y_train, num_epochs = iterations, minibatch_size = 128, learning_rate = learning_rate, lambd = lambd, h_layers = h_layers)
-            train_correctness = get_correctness(predict(X_train, h_layers, params), Y_train)
-            test_correctness = get_correctness(predict(X_test, h_layers, params), Y_test)
+                params, costs = model_adam(X_train, Y_train, num_epochs = iterations, minibatch_size = 128, learning_rate = learning_rate, lambd = lambd, keep_prob = keep_prob, h_layers = h_layers)            
+                #params = model(X_train, Y_train, num_epochs = iterations, minibatch_size = 128, learning_rate = learning_rate, lambd = lambd, h_layers = h_layers)
+                train_correctness = get_correctness(predict(X_train, h_layers, params), Y_train)
+                test_correctness = get_correctness(predict(X_test, h_layers, params), Y_test)
 
-            print("Deep NN. Hidden Layers: ", h_layers, ". Regularization lambda = ", lambd, ". # of iterations: ", iterations)
-            print("TRAIN. Correctness: ", train_correctness, " %")
-            print("TEST. Correctness: ", test_correctness, " %")
+                plot_cost(costs)
+
+                print("Deep NN. Hidden Layers: ", h_layers, ". Regularization lambda = ", lambd, ". # of iterations: ", iterations)
+                print("TRAIN. Correctness: ", train_correctness, " %")
+                print("TEST. Correctness: ", test_correctness, " %")
+                print("===============================")
             
-h_layers = [30, 20, 15, 10, 10, 5]
+h_layers = [60, 40, 30, 30]
 
-#tune_hyper_params(architectures=[h_layers], lambds=[0.7], iterations=150, learning_rate=0.001)
+#tune_hyper_params(architectures=[h_layers], lambds=[0], keep_probs = [.8], iterations=300, learning_rate=0.004)
 
-params = model_adam(X_train, Y_train, num_epochs = 500, minibatch_size = 128, learning_rate = 0.001, lambd = 0.7, h_layers = h_layers)            
+params, costs = model_adam(X_train, Y_train, num_epochs = 1000, minibatch_size = 128, learning_rate = 0.004, lambd = 0, keep_prob=0.8, h_layers = h_layers)            
 
 def imshow(X, i):
     plt.imshow(X[:, i].reshape(28,28))
     plt.show()
 
-def saveTestData(params, h_layers):
+def save_test_data(params, h_layers):
     test_data = np.genfromtxt('digits/datasets/test.csv', delimiter=',')
     #test_data = np.genfromtxt('digits/datasets/test_debug.csv', delimiter=',')
     X_result = test_data[1:, :].T
@@ -262,18 +285,17 @@ def saveTestData(params, h_layers):
 
     np.savetxt('digits/datasets/submition.csv', submit_data.astype(int), fmt='%i', delimiter=',', header='ImageId,Label')
 
+save_test_data(params, h_layers)
+plot_cost(costs)
+#============================================
+#Deep NN. Hidden Layers:  [30, 20, 15, 10, 10, 5] . Regularization lambda =  0 . # of iterations:  200
+#TRAIN. Correctness:  99.04722222222222  %
+#TEST. Correctness:  95.11666666666666  %
 
-saveTestData(params, h_layers)
+#cost after 50:  34.14852149414704 . Learning rate:  0.0026666666666666666
+#TRAIN. Correctness:  99.33333333333333  %
+#TEST. Correctness:  96.43333333333334  %
 
-#============ADAM===============
-#Deep NN. Hidden Layers:  [30, 20, 15, 10, 10, 5] . Regularization lambda =  0 . # of iterations:  300
-#TRAIN. Correctness:  99.9825  %
-#TEST. Correctness:  94.15  %
-
-#Deep NN. Hidden Layers:  [30, 20, 15, 10, 10, 5] . Regularization lambda =  1 . # of iterations:  300
-#TRAIN. Correctness:  97.97  %
-#TEST. Correctness:  95.35  % - 95.55 %
-
-#Deep NN. Hidden Layers:  [30, 20, 15, 10, 10, 5] . Regularization lambda =  0.7 . # of iterations:  300
-#TRAIN. Correctness:  98.63250000000001  %
-#TEST. Correctness:  95.8  %
+#TRAIN. Correctness:  99.88055555555556  %
+#TEST. Correctness:  95.36666666666666  %
+#cost after 1000:  10.06735110434442 . Learning rate:  0.00036363636363636367
